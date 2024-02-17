e9e01be436d23c3b289453abad28b20eb228b3c6
Jreyes <juanreyes1321@outlook.com>
2024-02-17 15:11:03 -0400

    [FEAT] Actualizacion de notas

--- my_library/search.py
+++ my_library/search.py
@@ -1,18 +1,25 @@
 
 data = [
-      'certificado de bachiller', 
-      'acta de nacimiento', 
-      'carta de buena conducta', 
-      'certificacion de estudios']
+    {'nombre': 'certificado de bachiller', 'extension': 'pdf', 'tipo': 'certificado', 'tamaño': '1MB', 'nota': ''},
+    {'nombre': 'acta de nacimiento', 'extension': 'pdf', 'tipo': 'documento oficial', 'tamaño': '500KB', 'nota': ''},
+    {'nombre': 'carta de buena conducta', 'extension': 'pdf', 'tipo': 'carta', 'tamaño': '300KB', 'nota': ''},
+    {'nombre': 'certificacion de estudios', 'extension': 'pdf', 'tipo': 'certificado', 'tamaño': '800KB', 'nota': ''},
+]
 
 def show_data():
     for item in data:
-            print(item)
+            print(f"Nombre: {item['nombre']}")
 
 def search_function(query):
 
     #búsqueda en una lista de elementos
-    data = ['certificado de bachiller', 'acta de nacimiento', 'carta de buena conducta', 'certificacion de estudios']
 
-    results = [item for item in data if query in item]
+    results = [item for item in data if query.lower() in item['nombre'].lower()]
     return results
+
+def update_document(nombre_documento, nueva_nota):
+    for documento in data:
+        if documento['nombre'] == nombre_documento:
+            documento['nota'] = nueva_nota
+            return "Documento actualizado"
+    return "Documento no encontrado"
---
+++ tests/readme.md
@@ -0,0 +1,18 @@
+# Project Title
+
+Unit tests para casos de usos en Python
+
+## Installation
+
+```linux
+
+sudo apt-get install python3-pytest -y
+
+
+## empezar tests
+
+python3 -m pytest -s tests
+
+
+Author: @j-Reyes132
+```
